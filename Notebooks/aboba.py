5a193f086bac7dc428bec72b14d4836e20a9d4f8
death-gr1ps <91392090+death-gr1ps@users.noreply.github.com>
2022-06-29 01:07:33 +0300

    Create aboba.py

---
+++ Notebooks/aboba.py
@@ -0,0 +1,13 @@
+Mounted at /content/gdrive
+17
+10
+
+
+
+---------------------------------------------------------------------------
+NameError                                 Traceback (most recent call last)
+<ipython-input-32-84e8748b7fe2> in <module>()
+      1 # save_total_grid(data, number = None, energy_res = None, gen_left_values = None, gen_right_values = [ [None, None], [None, None] ], path = None)
+----> 2 f_s.save_total_grid(results, number = data_number, energy_res = en, gen_left_values = gen_left_values, gen_right_values = gen_right_values)
+
+NameError: name 'en' is not defined
